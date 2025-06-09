df0499dd02d357f244c4be8d7d568939b60ce8f2
nominik <mike.nogai.spb@mail.ru>
2025-06-09 21:23:17 +0300

    change countMore

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        int[] prices = {13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30};
+
+        System.out.println("Для 31: " + countMore(prices, 31)); // 0
+        System.out.println("Для 26: " + countMore(prices, 26)); // 2
+        System.out.println("Для 25: " + countMore(prices, 25)); // 2
+        System.out.println("Для 20: " + countMore(prices, 20)); // 8
+    }
+
+    public static int countMore(int[] prices, int money) {
+        if (prices[0] > money) {
+            return prices.length; // все недоступны
         }
+
+        if (prices[prices.length - 1] < money) {
+            return 0; // все доступны
+        }
+
+        int left = 0;
+        int right = prices.length - 1;
+        while (left < right) {
+            int middle = (left + right) / 2;
+
+            if (prices[middle] > money && prices[middle - 1] <= money) {
+                return prices.length - middle;
+            } else if (prices[middle] <= money) {
+                left = middle + 1;
+                continue;
+            } else {
+                right = middle - 1;
+                continue;
+            }
+
+            // Ваш код:
+            // Если в middle первый недоступный товар, вернуть размер массива минус middle
+            // Если в middle доступный товар, то искать нужно правее - left = middle + 1
+            // Если в middle недоступный товар, то искать нужно левее - right = middle - 1
+
+        }
+        return 0; // иначе не скомпилится, джава сама не знает бинпоис
     }
 }
